38fffe07ff92648b3b070f308d86aeb1b3c14c25
Fabio <faac@ua.pt>
2020-11-02 01:17:10 +0000

    WalkMyAndroid--DONE

--- Lab03WalkMyAndroid/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
+++ Lab03WalkMyAndroid/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
@@ -12,15 +12,15 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int font = 0x7f030070;
-        public static final int fontProviderAuthority = 0x7f030072;
-        public static final int fontProviderCerts = 0x7f030073;
-        public static final int fontProviderFetchStrategy = 0x7f030074;
-        public static final int fontProviderFetchTimeout = 0x7f030075;
-        public static final int fontProviderPackage = 0x7f030076;
-        public static final int fontProviderQuery = 0x7f030077;
-        public static final int fontStyle = 0x7f030078;
-        public static final int fontWeight = 0x7f030079;
+        public static final int font = 0x7f030073;
+        public static final int fontProviderAuthority = 0x7f030075;
+        public static final int fontProviderCerts = 0x7f030076;
+        public static final int fontProviderFetchStrategy = 0x7f030077;
+        public static final int fontProviderFetchTimeout = 0x7f030078;
+        public static final int fontProviderPackage = 0x7f030079;
+        public static final int fontProviderQuery = 0x7f03007a;
+        public static final int fontStyle = 0x7f03007b;
+        public static final int fontWeight = 0x7f03007c;
     }
     public static final class bool {
         private bool() {}
@@ -30,10 +30,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f05003e;
-        public static final int notification_icon_bg_color = 0x7f05003f;
-        public static final int ripple_material_light = 0x7f05004a;
-        public static final int secondary_text_default_material_light = 0x7f05004c;
+        public static final int notification_action_color_filter = 0x7f050049;
+        public static final int notification_icon_bg_color = 0x7f05004a;
+        public static final int ripple_material_light = 0x7f050055;
+        public static final int secondary_text_default_material_light = 0x7f050057;
     }
     public static final class dimen {
         private dimen() {}
@@ -62,18 +62,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f070054;
-        public static final int notification_bg = 0x7f070055;
-        public static final int notification_bg_low = 0x7f070056;
-        public static final int notification_bg_low_normal = 0x7f070057;
-        public static final int notification_bg_low_pressed = 0x7f070058;
-        public static final int notification_bg_normal = 0x7f070059;
-        public static final int notification_bg_normal_pressed = 0x7f07005a;
-        public static final int notification_icon_background = 0x7f07005b;
-        public static final int notification_template_icon_bg = 0x7f07005c;
-        public static final int notification_template_icon_low_bg = 0x7f07005d;
-        public static final int notification_tile_bg = 0x7f07005e;
-        public static final int notify_panel_notification_icon_bg = 0x7f07005f;
+        public static final int notification_action_background = 0x7f070069;
+        public static final int notification_bg = 0x7f07006a;
+        public static final int notification_bg_low = 0x7f07006b;
+        public static final int notification_bg_low_normal = 0x7f07006c;
+        public static final int notification_bg_low_pressed = 0x7f07006d;
+        public static final int notification_bg_normal = 0x7f07006e;
+        public static final int notification_bg_normal_pressed = 0x7f07006f;
+        public static final int notification_icon_background = 0x7f070070;
+        public static final int notification_template_icon_bg = 0x7f070071;
+        public static final int notification_template_icon_low_bg = 0x7f070072;
+        public static final int notification_tile_bg = 0x7f070073;
+        public static final int notify_panel_notification_icon_bg = 0x7f070074;
     }
     public static final class id {
         private id() {}
@@ -83,31 +83,31 @@ public final class R {
         public static final int action_image = 0x7f080011;
         public static final int action_text = 0x7f080017;
         public static final int actions = 0x7f080018;
-        public static final int async = 0x7f08001e;
-        public static final int blocking = 0x7f080021;
-        public static final int chronometer = 0x7f080028;
-        public static final int forever = 0x7f080035;
-        public static final int icon = 0x7f08003a;
-        public static final int icon_group = 0x7f08003b;
-        public static final int info = 0x7f08003f;
-        public static final int italic = 0x7f080040;
-        public static final int line1 = 0x7f080041;
-        public static final int line3 = 0x7f080042;
-        public static final int normal = 0x7f08004b;
-        public static final int notification_background = 0x7f08004c;
-        public static final int notification_main_column = 0x7f08004d;
-        public static final int notification_main_column_container = 0x7f08004e;
-        public static final int right_icon = 0x7f080055;
-        public static final int right_side = 0x7f080056;
-        public static final int text = 0x7f080075;
-        public static final int text2 = 0x7f080076;
-        public static final int time = 0x7f08007a;
-        public static final int title = 0x7f08007b;
+        public static final int async = 0x7f080020;
+        public static final int blocking = 0x7f080024;
+        public static final int chronometer = 0x7f08002b;
+        public static final int forever = 0x7f080039;
+        public static final int icon = 0x7f08003e;
+        public static final int icon_group = 0x7f08003f;
+        public static final int info = 0x7f080044;
+        public static final int italic = 0x7f080045;
+        public static final int line1 = 0x7f080047;
+        public static final int line3 = 0x7f080048;
+        public static final int normal = 0x7f080051;
+        public static final int notification_background = 0x7f080052;
+        public static final int notification_main_column = 0x7f080053;
+        public static final int notification_main_column_container = 0x7f080054;
+        public static final int right_icon = 0x7f08005b;
+        public static final int right_side = 0x7f08005c;
+        public static final int text = 0x7f08007c;
+        public static final int text2 = 0x7f08007d;
+        public static final int time = 0x7f080081;
+        public static final int title = 0x7f080082;
     }
     public static final class integer {
         private integer() {}
 
-        public static final int status_bar_notification_info_maxnum = 0x7f090004;
+        public static final int status_bar_notification_info_maxnum = 0x7f090005;
     }
     public static final class layout {
         private layout() {}
@@ -122,7 +122,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0c0023;
+        public static final int status_bar_notification_info_overflow = 0x7f0c003d;
     }
     public static final class style {
         private style() {}
@@ -138,14 +138,14 @@ public final class R {
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] FontFamily = { 0x7f030072, 0x7f030073, 0x7f030074, 0x7f030075, 0x7f030076, 0x7f030077 };
+        public static final int[] FontFamily = { 0x7f030075, 0x7f030076, 0x7f030077, 0x7f030078, 0x7f030079, 0x7f03007a };
         public static final int FontFamily_fontProviderAuthority = 0;
         public static final int FontFamily_fontProviderCerts = 1;
         public static final int FontFamily_fontProviderFetchStrategy = 2;
         public static final int FontFamily_fontProviderFetchTimeout = 3;
         public static final int FontFamily_fontProviderPackage = 4;
         public static final int FontFamily_fontProviderQuery = 5;
-        public static final int[] FontFamilyFont = { 0x7f030070, 0x7f030078, 0x7f030079 };
+        public static final int[] FontFamilyFont = { 0x7f030073, 0x7f03007b, 0x7f03007c };
         public static final int FontFamilyFont_font = 0;
         public static final int FontFamilyFont_fontStyle = 1;
         public static final int FontFamilyFont_fontWeight = 2;
--- Lab03WalkMyAndroid/app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
+++ Lab03WalkMyAndroid/app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
@@ -15,14 +15,176 @@
  */
 package com.example.android.walkmyandroid;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.pm.PackageManager;
 
-public class MainActivity extends AppCompatActivity {
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
+    public static final int REQUEST_LOCATION_PERMISSION=1;
+    boolean mTrackingLocation;
+
+    //Location mLastLocation;
+    private LocationCallback mLocationCallback;
+    FusedLocationProviderClient mFusedLocationClient;
+
+
+    private Button mLocationButton;
+    TextView mLocationTextView;
+    ImageView mAndroidImageView;
+    AnimatorSet mRotateAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationButton = findViewById(R.id.button_location);
+        mLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mTrackingLocation)
+                    startTrackingLocation();
+                 else
+                    stopTrackingLocation();
+            }
+        });
+
+
+        mLocationTextView=findViewById(R.id.textview_location);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //Animating the Android pic
+        mAndroidImageView = (ImageView) findViewById(R.id.imageview_android);
+        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator
+                (this, R.animator.rotate);
+        mRotateAnim.setTarget(mAndroidImageView);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // If tracking is turned on, reverse geocode into an address
+                if (mTrackingLocation)
+                    new FetchAddressTask(MainActivity.this, MainActivity.this)
+                            .execute(locationResult.getLastLocation());
+        }
+    };
+    }
+
+
+    private void startTrackingLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            mFusedLocationClient.requestLocationUpdates
+                    (getLocationRequest(), mLocationCallback, null);
+            /*
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                /* Writes location in Latitude and Longitude Values
+                                mLastLocation = location;
+                                mLocationTextView.setText(
+                                        getString(R.string.location_text,
+                                                mLastLocation.getLatitude(),
+                                                mLastLocation.getLongitude(),
+                                                mLastLocation.getTime()));
+                                // Start the reverse geocode AsyncTask to transform lagitude and latitude to an address
+                                new FetchAddressTask(MainActivity.this,
+                                        MainActivity.this).execute(location);
+                            } else {
+                                mLocationTextView.setText(R.string.no_location);
+                            }
+                        }
+
+                    }); */
+        }
+        mLocationTextView.setText(getString(R.string.address_text,
+                getString(R.string.loading),
+                System.currentTimeMillis()));
+
+
+        mRotateAnim.start();
+        mTrackingLocation=true;
+        Button bt = findViewById(R.id.button_location);
+        bt.setText(R.string.stop_tracking_location);
+    }
+
+    /**
+     * Method that stops tracking the device. It removes the location
+     * updates, stops the animation and reset the UI.
+     */
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            mTrackingLocation = false;
+            mLocationButton.setText(R.string.start_tracking_location);
+            mLocationTextView.setText(R.string.textview_hint);
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+            mRotateAnim.end();
+
+        }
+    }
+
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+        @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode) {
+                case REQUEST_LOCATION_PERMISSION:
+                    // If the permission is granted, get the location,
+                    // otherwise, show a Toast
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        startTrackingLocation();
+                    } else {
+                        Toast.makeText(this,
+                                R.string.location_permission_denied,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+
+    @Override
+    public void onTaskCompleted(String result) {
+        // Update the UI
+        if (mTrackingLocation) {
+            mLocationTextView.setText(getString(R.string.address_text,
+                    result, System.currentTimeMillis()));
+        }
     }
 }
